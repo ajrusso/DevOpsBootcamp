95a183bc66fb4112e2fa63dfef753e1bfb47ca9d
BornSmasher <born.smasher@gmail.com>
2025-01-03 01:04:13 -0700

    Operating Systems & Linux Basics - Update Exercise 3

--- Exercises/OperatingSystemsAndLinuxBasics/Exercise_03/user_processes.sh
+++ Exercises/OperatingSystemsAndLinuxBasics/Exercise_03/user_processes.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 # Check all processes running for given user
-if [ -z "$1" ]; then
-    ps -aux
-else
-    ps -aux | grep $1
-fi
+echo "The processes running for user $USER:"
+ps aux | grep "$USER"
